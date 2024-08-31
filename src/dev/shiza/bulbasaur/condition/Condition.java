cb7d097e0bfe3176e77bb4417445534cba9d553e
shitzuu <hello@rafal.moe>
2024-08-31 03:21:04 +0200

    Add an option to wrap condition with parentheses

--- src/dev/shiza/bulbasaur/condition/Condition.java
+++ src/dev/shiza/bulbasaur/condition/Condition.java
@@ -5,12 +5,31 @@ public interface Condition {
 
   String generate();
 
-  default Condition and(final Condition condition) {
+  default Condition and(final Condition condition, final boolean parentheses) {
     final String generatedCondition = this.generate();
     if (generatedCondition.isEmpty()) {
       return condition;
     }
 
-    return () -> generatedCondition + " AND " + condition.generate();
+    final String generatedExpression = generatedCondition + " AND " + condition.generate();
+    return () -> parentheses ? "(" + generatedExpression + ")" : generatedExpression;
+  }
+
+  default Condition and(final Condition condition) {
+    return and(condition, false);
+  }
+
+  default Condition or(final Condition condition, final boolean parentheses) {
+    final String generatedCondition = this.generate();
+    if (generatedCondition.isEmpty()) {
+      return condition;
+    }
+
+    final String generatedExpression = generatedCondition + " OR " + condition.generate();
+    return () -> parentheses ? "(" + generatedExpression + ")" : generatedExpression;
+  }
+
+  default Condition or(final Condition condition) {
+    return or(condition, false);
   }
 }
